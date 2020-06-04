fc11afb11ee1c6eb981b6cb7a4e1f6c580d48dd0
ashiktantony <57289185+ashiktantony@users.noreply.github.com>
2020-06-04 15:24:30 +0530

    Updates

---
+++ precache-manifest.ac70b91bd6a6cb466a2da3c6d64b677c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd1fd52c297bbe1e555752beac46513b",
+    "url": "/Covid19-India-Staus/index.html"
+  },
+  {
+    "revision": "76d3fd9ce251fdcb4153",
+    "url": "/Covid19-India-Staus/static/css/2.30c2d978.chunk.css"
+  },
+  {
+    "revision": "adfca40d9a39e9aad999",
+    "url": "/Covid19-India-Staus/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "76d3fd9ce251fdcb4153",
+    "url": "/Covid19-India-Staus/static/js/2.57ee3f86.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Covid19-India-Staus/static/js/2.57ee3f86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adfca40d9a39e9aad999",
+    "url": "/Covid19-India-Staus/static/js/main.5770da72.chunk.js"
+  },
+  {
+    "revision": "662b5599095f99f27201",
+    "url": "/Covid19-India-Staus/static/js/runtime-main.fdc89131.js"
+  }
+]);
